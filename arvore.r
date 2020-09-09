52ecd2a851a4816f892563d24a91512a361a7d5e
Monique Schreiner <49883223+moniquesch@users.noreply.github.com>
2020-09-08 21:14:59 -0300

    Update arvore.r

--- arvore.r
+++ arvore.r
@@ -1,10 +1,10 @@
 library(ggimage)
-library(ggtree)
+library(ggtree) #BiocManager::install("ggtree")
 
-setwd("~/Desktop/analises-covid/bandeiras")
+setwd("~/Desktop/analises-covid")
 
 #carregando arvore
-tree = read.tree("arvore-brasil.tree")
+tree = read.tree("arvore-brasil.tree") #formato Newick
 
 estados <- tree$tip.label
 
@@ -12,11 +12,15 @@ caminhoImagens<-paste(lapply(estados, tolower), '.png', sep = "")
 info<- as.data.frame(cbind(estados, caminhoImagens))
 colnames(info)<- c('estados', 'caminho')
 
+setwd("~/Desktop/analises-covid/bandeiras")
+
 #documentação:
 #https://rdrr.io/bioc/ggtree/man/geom_tiplab.html
 p=ggtree(tree,branch.length = "none", ) %<+% info + xlim(NA, 13) + ylim(NA, 27)
-p + geom_tiplab(aes(image= caminho), geom="image", offset=0.5, align=T, size=.03, hjust=0,  asp = 1.6) +
-  geom_tiplab() +geom_tiplab(geom="label")
+p + geom_tiplab(aes(image= caminho), geom="image", offset=0.5, align=T, size=.03, hjust=0,  asp = 1.6) + 
+  geom_tiplab(geom="label")
+#aes: Aesthetic mappings (como variávies são mapeadas para propriedades visuais)
 #offset: distância da bandeira até a sigla    
 #size: tamanho da imagem
 #hjust: alinhamento horizontal da bandeira
+#asp = aspect ratio
